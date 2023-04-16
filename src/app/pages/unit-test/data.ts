d455c0e563afaab267591adcfb985d9b56f46190
st-linhdoan <mba0093@MBAM0093.local>
2023-04-16 19:38:21 +0700

    Fix exercise array increase

--- src/app/pages/unit-test/data.ts
+++ src/app/pages/unit-test/data.ts
@@ -1,5 +1,8 @@
 export const isArrIncrease = (arr: number[]) => {
-  for (let i = 0; i < arr.length - 1; i++){
+  if (!Array.isArray(arr) || arr.length < 2 || arr.some(isNaN)) {
+    return false;
+  }
+  for (let i = 0; i < arr.length - 1; i++) {
     if (arr[i+1] < arr[i]) {
       return false;
     }
